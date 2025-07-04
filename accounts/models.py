9c9dcde42f50056c1b54f13d11510550d6ca2e4f
José López <Josevld412@gmail.com>
2025-07-03 23:35:50 -0400

    Condicionales

--- accounts/models.py
+++ accounts/models.py
@@ -14,6 +14,7 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
